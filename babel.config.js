e7a46ab32df9b25615533f2bfc62b589fae991b0
Victor Zhuravlev <mocambe@mail.ru>
2019-12-23 16:36:58 +0300

    fix eslit warnings in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {
-        targets: {
-          node: 'current',
-        },
-      }],
-    ],
-  };
-  
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        node: 'current',
+      },
+    }],
+  ],
+};
