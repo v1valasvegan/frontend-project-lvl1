8cfd6856153d4e23b371b6af11a345a0390cf724
Victor Zhuravlev <mocambe@mail.ru>
2019-12-23 18:53:08 +0300

    remove innecessary fie brain-games.js

--- src/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import ask from '../index';
-
-const greeting = 'Welcome to The Brain Games!';
-console.log(greeting);
-const userName = ask('May I have your name? ')();
-console.log(`Hello, ${userName}!`);
