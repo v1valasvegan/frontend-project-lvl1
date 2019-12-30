2e4d088c4efc0d078dd21e2878a1f04b52ae9449
Victor Zhuravlev <mocambe@mail.ru>
2019-12-30 19:02:15 +0300

    refactor. move makeQuestion function to module level, rename answerIndex const

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,25 +7,24 @@ const sequenceLength = 10;
 const startRange = cons(0, 20);
 const incrementRange = cons(1, 15);
 
+const makeQuestion = (start, increment, answer) => {
+  const iter = (i, result) => {
+    if (i === start + sequenceLength * increment) {
+      return result;
+    }
+    const newResult = i === answer ? `${result}.. ` : `${result}${i} `;
+    return iter(i + increment, newResult);
+  };
+
+  return iter(start, '');
+};
+
 const makeGameData = () => {
   const start = randomInt(car(startRange), cdr(startRange));
   const increment = randomInt(car(incrementRange), cdr(incrementRange));
-  const missedNumberIndex = randomInt(0, sequenceLength - 1);
-  const answer = start + missedNumberIndex * increment;
-
-  const makeQuestion = () => {
-    const iter = (i, result) => {
-      if (i === start + sequenceLength * increment) {
-        return result;
-      }
-      const newResult = i === answer ? `${result}.. ` : `${result}${i} `;
-      return iter(i + increment, newResult);
-    };
-
-    return iter(start, '');
-  };
-
-  return cons(makeQuestion(), answer.toString());
+  const answerIndex = randomInt(0, sequenceLength - 1);
+  const answer = start + answerIndex * increment;
+  return cons(makeQuestion(start, increment, answer), answer.toString());
 };
 
 export default play(description, makeGameData);
